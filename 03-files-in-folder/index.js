f75a3a404c58b84143b5a6403a5dfe8f5930a67b
kirkor313 <kirkor313@mail.ru>
2024-01-21 16:08:40 +0300

    feat: add task solution '03-files-in-folder'

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,24 @@
+const fs = require ('fs');
+const path = require('path');
+const pathToSecretFolder = path.join(__dirname, 'secret-folder');
+
+fs.readdir(pathToSecretFolder,
+  { withFileTypes: true },
+  (err, files) => {
+  console.log("\nФайлы текущей директории:");
+  if (err) throw err;
+  files.forEach(file => {
+    if (file.isFile()) {
+      const pathToFile = path.join(pathToSecretFolder, file.name)
+      fs.stat(pathToFile,
+        (err,stats) => {
+          if (err) throw err;
+          const res = file.name.split('.')
+          const name = res.slice(0, res.length-1).join('.');
+          const extension = res[res.length - 1];
+          const size = stats.size / 1000;
+          console.log(`${name} - ${extension} - ${size} kb`)
+      })
+    }
+  })
+})
